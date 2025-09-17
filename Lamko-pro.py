2d2832784cacc4e858df2bfbbf2ecf0ad4a423eb
Smolite <shinnara.yuchan@gmail.com>
2025-09-17 17:22:45 +0900

    Update Lamko-pro.py

--- Lamko-pro.py
+++ Lamko-pro.py
@@ -316,18 +316,6 @@ def masked_accuracy(y_true, y_pred):
     pred_id = tf.argmax(y_pred, axis=-1, output_type=tf.int32)
     acc = tf.cast(tf.equal(y_true, pred_id), tf.float32) * mask
     return tf.reduce_sum(acc) / (tf.reduce_sum(mask) + 1e-8)
-
-def masked_perplexity(y_true, y_pred, eps=0.1):
-    y_true = tf.cast(y_true, tf.int32)
-    mask = tf.cast(tf.not_equal(y_true, pad_id), tf.float32)
-    vocab = tf.shape(y_pred)[-1]
-    y_true_oh = tf.one_hot(y_true, depth=vocab, dtype=tf.float32)
-    y_true_ls = (1.0 - eps) * y_true_oh + eps / tf.cast(vocab, tf.float32)
-    log_probs = tf.nn.log_softmax(y_pred, axis=-1)
-    per_tok = -tf.reduce_sum(y_true_ls * log_probs, axis=-1)
-    per_tok = per_tok * mask
-    mean_loss = tf.reduce_sum(per_tok) / (tf.reduce_sum(mask) + 1e-8)
-    return tf.exp(mean_loss)
 # =======================
 # 모델 생성 & 학습
 # =======================
@@ -338,7 +326,7 @@ with strategy.scope():
     model.summary()
 
     optimizer = tf.keras.optimizers.Adam(3e-4, beta_1=0.9, beta_2=0.95, epsilon=1e-8, clipnorm=1.0)
-    model.compile(optimizer=optimizer, loss=smoothed_loss_keras, metrics=[masked_accuracy, masked_perplexity])
+    model.compile(optimizer=optimizer, loss=smoothed_loss_keras, metrics=[masked_accuracy])
     history = model.fit(dist_dataset, epochs=1, verbose=1)
 
 # =======================
